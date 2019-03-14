d6963af77cf95d33b2ce4292d16a57b824026806
Maxim Sinelnikov <maxim@sinelnikov.info>
2019-03-14 20:13:52 +0100

    authors

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -15,17 +15,42 @@ class AddBook extends Component {
   displayAuthors() {
     let data = this.props.data;
     if (data.loading) {
-      return <div>Loading books...</div>;
+      return <option disabled>Loading Authors...</option>;
     } else {
-      return data.books.map(book => <li key={book.id}>{book.name}</li>);
+      return data.authors.map(author => (
+        <option key={author.id} value={author.id}>
+          {author.name}
+        </option>
+      ));
     }
   }
 
   render() {
     return (
-      
+      <form id="add-book">
+        <div className="field">
+          <label>Book name</label>
+          <input type="text" />
+        </div>
+
+        <div className="field">
+          <label>Genre:</label>
+          <input type="text" />
+        </div>
+
+        <div className="field">
+          <label>Author:</label>
+
+          <select>
+            <option>Select author</option>
+            {this.displayAuthors()}
+          </select>
+        </div>
+
+        <button>+</button>
+      </form>
     );
   }
 }
 
-export default graphql(getBooksQuery)(AddBook);
+export default graphql(getAuthorsQuery)(AddBook);
